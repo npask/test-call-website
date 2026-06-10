189f5256b45eb358b2dd3b6c9008228a3d50653d
npask <87610771+npask@users.noreply.github.com>
2026-06-10 17:02:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,18 +4,51 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
-// WICHTIG: server direkt an socket.io
 const io = new Server(server, {
     cors: {
         origin: "*"
     }
 });
 
+
 app.use(express.static("public"));
 
+const users = {}; // socket.id -> {username, avatar}
+
 io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+
+    socket.on("join", (data) => {
+
+        users[socket.id] = {
+            username: data.username,
+            avatar: data.avatar
+        };
+
+        socket.broadcast.emit("user-joined", {
+            id: socket.id,
+            ...users[socket.id]
+        });
+
+        io.emit("users", users);
+    });
+
+    socket.on("signal", (data) => {
+        io.to(data.to).emit("signal", {
+            from: socket.id,
+            signal: data.signal
+        });
+    });
+
+    socket.on("disconnect", () => {
+
+        delete users[socket.id];
+
+        io.emit("user-left", socket.id);
+        io.emit("users", users);
+    });
+
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(3000, () => {
+    console.log("http://localhost:3000");
+});
